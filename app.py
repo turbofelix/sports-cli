6d9b4811afbbf6cf8ede8c8a2690bc9907c6c673
turbofelix <f.mantei96@gmail.com>
2024-02-14 00:02:26 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import click
+
+from sportscli.cli import _test_function
+
+@click.group()
+def cli():
+    _test_function()
+    pass
+
+@click.command()
+def hello():
+    click.echo("Hello world!")
+
+cli.add_command(hello)
+
+if __name__ == '__main__':
+    cli()
